ab1fb777f4f70619643a06c353c8a449c0fdf158
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-12 21:02:44 -0500

    commit tree 5d06a4c2bd97a03418149573136e7bd9c8ccc246

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,36 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name, :save
+    attr_reader :all, :print_all
+
+    @@all = []
+    @@print_all = []
+    @save = []
+
+    def initialize(pup)
+        @@print_all << pup
+        @name = pup
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+        @@print_all.clear
+        @save.clear
+    end
+
+    def self.print_all
+        puts @@print_all.join("\n")
+    end
+
+    def save
+        @@all << self
+    end
+
+end
